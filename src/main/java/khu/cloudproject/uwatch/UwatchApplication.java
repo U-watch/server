9c99d0e181d9d54a49cfbefc1454054dff4cf388
yxhwxn <officeyh9819@gmail.com>
2024-10-02 13:38:37 +0900

    Init Project

---
+++ src/main/java/khu/cloudproject/uwatch/UwatchApplication.java
@@ -0,0 +1,13 @@
+package khu.cloudproject.uwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/khu/cloudproject/uwatch/UwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package khu.cloudproject.uwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
