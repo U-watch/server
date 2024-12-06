c761c75ade96a8551e0f78344ee3cf4dadb05ff8
yxhwxn <officeyh9819@gmail.com>
2024-12-06 13:16:04 +0900

    Feat: HealthCheck 추가

---
+++ src/main/java/khu/cloudproject/uwatch/global/HealthCheck.java
@@ -0,0 +1,13 @@
+package khu.cloudproject.uwatch.global;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheck {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK, healthy!";
+    }
+}
